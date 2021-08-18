ec1dd8319f70e7d34ae7c0245a18d8fb3ba78434
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-08-18 15:23:33 +1000

    fixed  upload image bug and footer font size

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,57 +3,66 @@ import './footer.css';
 import Logo from './../../assets/Logos/LBS_Logo_Flat_Red.jpg';
 import GoogleButton from '../../assets/Images/GooglePlayButton.png';
 import AppleButton from '../../assets/Images/AppStoreButton.png';
+import { SvgIcon } from '@material-ui/core';
 
 export default function footer() {
   return (
-    <div className="Footer">  
+    <div className="Footer">
       <div className="FooterBar Advert">
         <div className="FooterWrapper">
-          <div><img src={Logo} alt="logo" className="FooterLogo"/></div>
+          <div><img src={Logo} alt="logo" className="FooterLogo" /></div>
           <div className="TextWrapper">
             <div className="LargeText">Little Big Shed in your Pocket!</div>
             <div className="RegularText">Download our mobile app to borrow and lend, where ever you are!</div>
           </div>
           <div className="AppButtonsSection">
-            <img src={AppleButton} alt="Google Play" className="AppButton"/>
-            <img src={GoogleButton} alt="Google Play" className="AppButton"/>
+            <img src={AppleButton} alt="Google Play" className="AppButton" />
+            <img src={GoogleButton} alt="Google Play" className="AppButton" />
           </div>
 
         </div>
       </div>
 
       <div className="FooterBar Links">
-        <div className="FooterWrapper" style={{justifyContent: "space-around"}}>
+        <div className="FooterWrapper" style={{ justifyContent: "space-around" }}>
           <div>
             <div className="LinkHeader">Account</div>
-            <div>Support Team</div>
-            <div>Settings</div>
-            <div>Availability</div>
-            <div>My Shed</div>
-          </div>
+            <div className="LinkText">
+              <div>Support Team</div>
+              <div>Settings</div>
+              <div>Availability</div>
+              <div>My Shed</div>
+            </div>
 
+          </div>
           <div>
             <div className="LinkHeader">Conditions</div>
-            <div>Terms and Conditions</div>
-            <div>Privacy Policy</div>
-            <div>Location Policy</div>
-            <div>Conditions</div>
+            <div className="LinkText">
+              <div>Terms and Conditions</div>
+              <div>Privacy Policy</div>
+              <div>Location Policy</div>
+              <div>Conditions</div>
+            </div>
           </div>
 
           <div>
             <div className="LinkHeader">Discover</div>
-            <div>New Items</div>
-            <div>Top Items</div>
-            <div>Search</div>
-            <div>Discover</div>
+            <div className="LinkText">
+              <div>New Items</div>
+              <div>Top Items</div>
+              <div>Search</div>
+              <div>Discover</div>
+            </div>
           </div>
 
           <div>
             <div className="LinkHeader" >Top Categories</div>
-            <div>Automotive</div>
-            <div>Gardening</div>
-            <div>Hand Tools</div>
-            <div>Power Tools</div>
+            <div className="LinkText">
+              <div>Automotive</div>
+              <div>Gardening</div>
+              <div>Hand Tools</div>
+              <div>Power Tools</div>
+            </div>
           </div>
 
         </div>
