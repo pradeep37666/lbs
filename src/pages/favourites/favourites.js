ca06f31c1e02ee033ed7834bd494f5b5e0523190
Roba <roba1818@gmail.com>
2021-09-01 11:17:31 +1000

    Displaying favourite in 'favourites page'

--- src/pages/favourites/favourites.js
+++ src/pages/favourites/favourites.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import instance from '../../util/axios';
 import './favourites.css'
 import PageWrapper from '../../components/pageWrapper/pageWrapper'
 import UserShedNav from '../../components/UserShedNav/UserShedNav'
+import ItemCard from '../../components/itemCard/itemCard';
 
 export default function Favourites() {
 
     const [accountContent, setAccountContent] = useState('Favourites')
-
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        instance.get('/liked/findByUid')
+          .then(({data} ) => {
+            console.log("response",data)
+            setItems(data);
+            setLoading(false);
+          })
+          .catch(()=>{
+              
+          })
+    },[])
     return (
         <PageWrapper>
             <div className="UserShedWrapper">
@@ -16,7 +30,15 @@ export default function Favourites() {
                 <div className="UserShed__Title">
                     {accountContent}
                 </div>
-
+                <div className="ItemCardSection">
+                    { loading 
+                    ? <div>Loading items...</div>
+                    :               
+                     items.map((item, i) => {
+                    return <ItemCard item={item} key={i}/>
+                    })
+                    }
+                </div>
                 
 
             </div>
