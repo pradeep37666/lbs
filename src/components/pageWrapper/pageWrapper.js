e644ef155fa56483104b46b5b7187a769c520011
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-10-05 17:50:46 +1000

    added drawer to trade calendar

--- src/components/pageWrapper/pageWrapper.js
+++ src/components/pageWrapper/pageWrapper.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './pageWrapper.css';
 import Header from './../header/header.js';
-<<<<<<< HEAD
 import Footer from '../footer/footer'
-=======
-import Footer from '../footer/footer.js'
->>>>>>> trades
 
 const pageWrapper = (props) => {
   return (
