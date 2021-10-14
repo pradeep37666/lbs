6df4869b6f787cfdc423d820088f3679dc167cb4
AnthonyCarroll97 <anthonycarroll1997@hotmail.com>
2021-10-14 16:50:12 +1000

    added no content component

--- src/pages/favourites/favourites.js
+++ src/pages/favourites/favourites.js
@@ -5,44 +5,58 @@ import PageWrapper from '../../components/pageWrapper/pageWrapper'
 import UserShedNav from '../../components/UserShedNav/UserShedNav'
 import ItemCard from '../../components/itemCard/itemCard';
 import { isMobile } from 'react-device-detect';
+import { CircularProgress } from '@material-ui/core';
+import NoContent from '../../components/NoContent/NoContent';
+import { useHistory } from 'react-router';
 
 export default function Favourites() {
-
+    const history = useHistory()
     const [accountContent, setAccountContent] = useState('Favourites')
     const [items, setItems] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(()=>{
         instance.get('/liked/findByUid')
           .then(({data} ) => {
             setItems(data);
-            setLoading(false);
+            setIsLoading(false);
           })
           .catch(()=>{
               
           })
     },[])
+
+    const renderItems = () => {
+        return items.map((item, i) => {
+            return <ItemCard item={item} key={i} favourited={true}/>
+            })
+    }
     return (
         <PageWrapper>
             <div className="UserShedWrapper">
-            {!isMobile && <UserShedNav setAccountContent={setAccountContent} accountContent={accountContent}/>}
-
-            <div className="UserShed__MainContent">
-                <div className="UserShed__Title">
-                    {accountContent}
+                {!isMobile && <UserShedNav setAccountContent={setAccountContent} accountContent={accountContent}/>}
+                <div className="UserShed__MainContent" style={isLoading ? { display: 'flex', justifyContent: 'center', alignItems: 'center'} : null}>
+                    {isLoading ? (
+                        <CircularProgress />
+                    ) : (
+                        items.length === 0 ? (
+                            <NoContent 
+                            header="No favourites" 
+                            text="Items that you favourite will appear here"
+                            buttonText="Search for an item" 
+                            onButtonClick={() => history.push('/search')}/>
+                        ) : (
+                            <>
+                                <div className="UserShed__Title">
+                                    {accountContent}
+                                </div>
+                                <div className="ItemCardSection">
+                                    { renderItems() }
+                                </div>
+                            </>
+                        )
+                    )}
                 </div>
-                <div className="ItemCardSection">
-                    { loading 
-                    ? <div>Loading items...</div>
-                    :               
-                     items.map((item, i) => {
-                    return <ItemCard item={item} key={i} favourited={true}/>
-                    })
-                    }
-                </div>
-                
-
-            </div>
-
             </div>
         </PageWrapper>
     )
--- src/pages/trades/trades.js
+++ src/pages/trades/trades.js
@@ -6,30 +6,35 @@ import Instance from '../../util/axios'
 import TradeCalendar from '../../components/tradeCalendar/TradeCalendar'
 import TradeSidebar from '../../components/TradeSidebar/TradeSidebar'
 import { isMobile } from 'react-device-detect'
-import { SwipeableDrawer } from '@material-ui/core'
+import { CircularProgress, SwipeableDrawer } from '@material-ui/core'
 import TradeFailed from '../../components/TradeFailed/TradeFailed'
 import userEvent from '@testing-library/user-event'
 import useGlobalState from '../../util/useGlobalState'
-import ReviewModal from '../../components/reviewModal/reviewModal'
+
 import ReviewTrade from '../../components/ReviewTrade/ReviewTrade'
+import { useHistory } from 'react-router'
+import NoContent from '../../components/NoContent/NoContent'
 
 export default function Trades() {
     const { state } = useGlobalState()
     const { user } = state
+    const history = useHistory()
     const [reportModalVisible, setReportModalVisible] = useState(false)
     const [reviewModalVisible, setReviewModalVisible] = useState(false)
     const [accountContent, setAccountContent] = useState('Trades')
     const [selectedBooking, setSelectedBooking] = useState(null)
     const [lenderBookingItems, setLenderBookingItems] = useState([])
     const [borrowerBookingItems, setBorrowerBookingItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         getBookings()
     },[])
 
-    const getBookings = () => {
-        getLenderBookings()
-        getBorrowerBookings()
+    const getBookings = async () => {
+        await getLenderBookings()
+        await getBorrowerBookings()
+        setIsLoading(false)
     }
 
     const getLenderBookings = async () => {
@@ -59,7 +64,7 @@ export default function Trades() {
         return filteredBookings
      }
 
-
+    const noBookings = (lenderBookingItems.length === 0 && borrowerBookingItems.length === 0)
     return (
         <PageWrapper>
             { reportModalVisible &&  
@@ -72,21 +77,36 @@ export default function Trades() {
             <div className="UserShedWrapper">
                 { !isMobile && <UserShedNav setAccountContent={setAccountContent} accountContent={accountContent}/>}
 
-                <div className="TradesContainer">
-                    <div className="UserShed__Title">
-                        {accountContent}
-                    </div>
-                   
-                    <TradeCalendar 
-                    setSelectedBooking={setSelectedBooking}
-                    lenderBookingItems={lenderBookingItems}
-                    borrowerBookingItems={borrowerBookingItems}
-                    />
+                <div className="TradesContainer" style={ isLoading ? { display: 'flex', justifyContent: 'center', alignItems: 'center'} : noBookings ? { width: '100%'} : null}>
+                    { isLoading ? (
+                        <CircularProgress color="inherit" />
+                    ) : (
+                        noBookings ? (
+                            <NoContent 
+                            header="No Trades"
+                            buttonText="Search for items" 
+                            text="You currently don't have any active or upcoming trades, borrow or lend an item to get started." 
+                            onButtonClick={() => history.push('/search')} />
+                        ) : (
+                            <>
+                                <div className="UserShed__Title">
+                                    {accountContent}
+                                </div>
+                        
+                                <TradeCalendar 
+                                setSelectedBooking={setSelectedBooking}
+                                lenderBookingItems={lenderBookingItems}
+                                borrowerBookingItems={borrowerBookingItems}
+                                /> 
+                            </> 
+                        )
+                        
+                    )}
                 </div>
                 { isMobile ? (
-                <SwipeableDrawer anchor='right' open={selectedBooking} onClose={() => setSelectedBooking(null)}>
-                    { selectedBooking && <TradeSidebar getBookings={getBookings} booking={selectedBooking} />}
-                </SwipeableDrawer>
+                    <SwipeableDrawer anchor='right' open={selectedBooking} onClose={() => setSelectedBooking(null)}>
+                        { selectedBooking && <TradeSidebar getBookings={getBookings} booking={selectedBooking} />}
+                    </SwipeableDrawer>
                 ) : (
                      selectedBooking && <TradeSidebar getBookings={getBookings} booking={selectedBooking} setReportModalVisible={setReportModalVisible} setReviewModalVisible={setReviewModalVisible}/>
                 )}
