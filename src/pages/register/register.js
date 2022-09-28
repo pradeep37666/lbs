1754fc44b3a49d03197941d4b189b1ce11e16f2f
RyotoTheMoon <ryo@moonward.com.au>
2022-09-28 10:54:57 +1000

    added google analytics

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -17,7 +17,21 @@ import registerReducer from '../../util/reducers/registerReducer'
 import { REGISTER_PAGES, SNACKBAR_BUTTON_TYPES } from '../../assets/Data/LBSEnum'
 import { getPrevRegisterPage } from '../../util/getPrevPage'
 import useErrorState from '../../util/reducers/errorContext'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics, logEvent } from 'firebase/analytics'
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 const FormContext = createContext()
 
 export default function Register() {
@@ -110,11 +124,12 @@ export default function Register() {
         try{
             const { data, status } = await Instance.post(isLenderUpgrade ? '/auth/lenderSignUp' : '/auth/signUp', signupDetails)
             if(status === 201) {
-                globalDispatch({ type: 'setUser', data: data.user})
+                globalDispatch({ type: 'setUser', data: data.user })
                 localStorage.setItem('LBSToken', data.token.accessToken)
                 await registerCometChat(data.user)
                 setIsRegisterLoading(false)
                 dispatch({ type: 'setCurrentPage', data: REGISTER_PAGES.COMPLETE})
+                logEvent(analytics, "user_register_web")
             }
         } catch(e) {
             console.log(e.response)
