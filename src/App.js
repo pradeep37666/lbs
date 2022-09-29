ea9e8ce3f20135a418b8c748fd0871308d20763f
George <george.hawk88@gmail.com>
2022-09-29 14:15:51 +1000

    fix: :sparkles: search page api

--- src/App.js
+++ src/App.js
@@ -30,11 +30,7 @@ import UpdatePassword from './pages/account/UpdatePassword/UpdatePassword'
 import ScrollToTop from './util/ScrollToTop'
 import Application from './pages/application/Application'
 import InvalidRoutePage from './pages/InvalidRoutePage'
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-} from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import reducer from './util/reducers/globalStateReducer'
 import snackbarReducer from './util/reducers/snackbarReducer'
 import { CometChat } from '@cometchat-pro/chat'
@@ -49,12 +45,12 @@ import LenderShed from './pages/lender-shed/lender-shed'
 const SentryRoute = Sentry.withSentryRouting(Route)
 const history = createBrowserHistory()
 Sentry.init({
-  integrations: [
-    new BrowserTracing({
-      routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
-    }),
-  ],
-  tracesSampleRate: 1.0,
+	integrations: [
+		new BrowserTracing({
+			routingInstrumentation: Sentry.reactRouterV5Instrumentation(history),
+		}),
+	],
+	tracesSampleRate: 1.0,
 })
 
 export const GlobalStateContext = React.createContext()
@@ -62,164 +58,188 @@ export const GlobalErrorContext = React.createContext()
 
 const initialState = {}
 const initialErrorState = {
-  toggleSnackbar: false,
-  snackbarMessage: '',
-  snackbarBtnText: '',
-  snackbarBtnFunc: () => {}
+	toggleSnackbar: false,
+	snackbarMessage: '',
+	snackbarBtnText: '',
+	snackbarBtnFunc: () => {},
 }
 
 // const stripe = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
-const stripe = loadStripe('pk_test_51JAi4wAaysFIruTL4KcRCjRZ1aSi6KYbnBDjGRkKuuht6nojLH2ItABIh430Ee9nHzbnqgm80dlwWJZfXMLrbATw00fg4d8m5c')
+const stripe = loadStripe(
+	'pk_test_51JAi4wAaysFIruTL4KcRCjRZ1aSi6KYbnBDjGRkKuuht6nojLH2ItABIh430Ee9nHzbnqgm80dlwWJZfXMLrbATw00fg4d8m5c'
+)
 
 function App() {
-  const [ state, dispatch ] = useReducer(reducer, initialState)
-  const [ errorState, errorDispatch ] = useReducer(snackbarReducer, initialErrorState)
-  const [ loadingUser, setLoadingUser ] = useState(true)
-  const { user } = state
-  const token = localStorage.getItem('LBSToken')
+	const [state, dispatch] = useReducer(reducer, initialState)
+	const [errorState, errorDispatch] = useReducer(
+		snackbarReducer,
+		initialErrorState
+	)
+	const [loadingUser, setLoadingUser] = useState(true)
+	const { user } = state
+	const token = localStorage.getItem('LBSToken')
 
-  useEffect(() => {
-    if (!token) {
-      setLoadingUser(false)
-      return
-    } 
-    setupCometChat()
-    getCurrentUser()
-  }, [])
-  
-  useEffect(() => {
-    user && getUnReadMessageCount()
-  },[user])
+	useEffect(() => {
+		if (!token) {
+			setLoadingUser(false)
+			return
+		}
+		setupCometChat()
+		getCurrentUser()
+	}, [])
 
-  const getCurrentUser = async () => {
-    try{
-      const { data } = await Instance.get('/users/me')
-      dispatch({ type: 'setUser', data })
-    } catch(err) {
-      localStorage.removeItem('LBSToken')
-    } finally {
-      setLoadingUser(false)
-    }
-  }
+	useEffect(() => {
+		user && getUnReadMessageCount()
+	}, [user])
 
-  const setupCometChat = () => {
-    const appId = process.env.REACT_APP_CHAT_APP_ID
-    let cometChatSettings = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion('us').build();
-    CometChat.init(appId, cometChatSettings)
-    .then(
-      () => {
-        console.log("Initialization completed successfully");
-       },
-       error => {
-        console.log("Initialization failed with error:", error);
-      }
-    );
-  }
+	const getCurrentUser = async () => {
+		try {
+			const { data } = await Instance.get('/users/me')
+			dispatch({ type: 'setUser', data })
+		} catch (err) {
+			localStorage.removeItem('LBSToken')
+		} finally {
+			setLoadingUser(false)
+		}
+	}
 
-  const getUnReadMessageCount = async () => {
-    try {
-      const cometUser = await CometChat.getUser(user?.id)
-      if (!cometUser) return
-      const countObject = await CometChat.getUnreadMessageCount()
-      if (typeof Object.values(countObject?.users)[0] === 'undefined') return
-      dispatch({
-        type: 'setUnReadMessageCount',
-        data: Object.values(countObject?.users)[0]
-      })
-    } catch (error) {
-      console.log({error})
-    } 
-  }
+	const setupCometChat = () => {
+		const appId = process.env.REACT_APP_CHAT_APP_ID
+		let cometChatSettings = new CometChat.AppSettingsBuilder()
+			.subscribePresenceForAllUsers()
+			.setRegion('us')
+			.build()
+		CometChat.init(appId, cometChatSettings).then(
+			() => {
+				console.log('Initialization completed successfully')
+			},
+			(error) => {
+				console.log('Initialization failed with error:', error)
+			}
+		)
+	}
 
-  function AuthRoute({ component: Component, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={(props) =>
-          user
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
-      />
-    )
-  }
+	const getUnReadMessageCount = async () => {
+		try {
+			const cometUser = await CometChat.getUser(user?.id)
+			if (!cometUser) return
+			const countObject = await CometChat.getUnreadMessageCount()
+			if (typeof Object.values(countObject?.users)[0] === 'undefined') return
+			dispatch({
+				type: 'setUnReadMessageCount',
+				data: Object.values(countObject?.users)[0],
+			})
+		} catch (error) {
+			console.log({ error })
+		}
+	}
 
-  function AuthRedirectRoute({ component: Component, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={(props) =>
-          user
-            ? <Redirect to={{ pathname: '/user/account', state: { from: props.location } }} />
-            : <Component {...props} />}
-      />
-    )
-  }
+	function AuthRoute({ component: Component, ...rest }) {
+		return (
+			<Route
+				{...rest}
+				render={(props) =>
+					user ? (
+						<Component {...props} />
+					) : (
+						<Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+					)
+				}
+			/>
+		)
+	}
 
-  function RedirectBecomeLender({ component: Component, ...rest }) {
-    return (
-      <Route
-        {...rest}
-        render={(props) =>
-          user && user.isLender
-            ? <Redirect to={{ pathname: '/user/account', state: { from: props.location } }} />
-            : <Component {...props} />}
-      />
-    )
-  }
+	function AuthRedirectRoute({ component: Component, ...rest }) {
+		return (
+			<Route
+				{...rest}
+				render={(props) =>
+					user ? (
+						<Redirect
+							to={{ pathname: '/user/account', state: { from: props.location } }}
+						/>
+					) : (
+						<Component {...props} />
+					)
+				}
+			/>
+		)
+	}
 
-  return (
-    <Elements stripe={stripe}>
-      <GlobalErrorContext.Provider value={{errorState, errorDispatch}}>
-      <GlobalStateContext.Provider value={{ state, dispatch }}>
-        {loadingUser ? '' : 
-        <Router history={history}>
-          <LBSSnackBar timeout={10000}/>
-          <ScrollToTop>
-            {/* marketing pages here with different routers */}
-            <SentryRoute exact path='/' component={Top} />
-            <SentryRoute exact path='/lend_your_stuff' component={LendYourStuff} />
-            <SentryRoute exact path='/rent_stuff' component={RentStuff} />
-            <SentryRoute exact path='/how_it_works' component={HowItWorks} />
-            <SentryRoute exact path='/about_us' component={AboutUs} />
-            <SentryRoute exact path='/blog' component={Blog} />
-            <SentryRoute exact path='/protection' component={Protection} />
-            <SentryRoute exact path='/faqs' component={FAQs} />
-            <SentryRoute exact path='/contact_us' component={ContactUs} />
+	function RedirectBecomeLender({ component: Component, ...rest }) {
+		return (
+			<Route
+				{...rest}
+				render={(props) =>
+					user && user.isLender ? (
+						<Redirect
+							to={{ pathname: '/user/account', state: { from: props.location } }}
+						/>
+					) : (
+						<Component {...props} />
+					)
+				}
+			/>
+		)
+	}
 
-            <SentryRoute exact path="/home" component={Home} />
-            <SentryRoute exact path="/item/:itemId" component={ItemPage} />
-            <SentryRoute exact path="/search/:searchParams?" component={Search} />
-            <SentryRoute exact path="/forgotpassword" component={ForgotPassword} />
-            <AuthRoute exact path="/item/edit/:itemId" component={EditItemPage} />
-            <AuthRoute path="/user/trades" component={TradesPage} />
-            <AuthRoute path="/user/messages" component={Messages} />
-            <AuthRoute path="/user/your_shed" component={YourshedPage} />
-            <AuthRoute path="/user/favourites" component={FavouritesPage} />
-            <AuthRoute path="/user/account" component={AccountPage} />
-            <AuthRoute path="/user/update_password" component={UpdatePassword} />
-            {/* if the user is already a lender they should be unable to access the upgrade to lender page */}
-            <RedirectBecomeLender path="/user/upgrade_to_lender" component={UpgradeLender} />
-            <AuthRoute path="/item/:itemId/application" component={Application} />
+	return (
+		<Elements stripe={stripe}>
+			<GlobalErrorContext.Provider value={{ errorState, errorDispatch }}>
+				<GlobalStateContext.Provider value={{ state, dispatch }}>
+					{loadingUser ? (
+						''
+					) : (
+						<Router history={history}>
+							<LBSSnackBar timeout={10000} />
+							<ScrollToTop>
+								{/* marketing pages here with different routers */}
+								<SentryRoute exact path='/' component={Top} />
+								<SentryRoute exact path='/lend_your_stuff' component={LendYourStuff} />
+								<SentryRoute exact path='/rent_stuff' component={RentStuff} />
+								<SentryRoute exact path='/how_it_works' component={HowItWorks} />
+								<SentryRoute exact path='/about_us' component={AboutUs} />
+								<SentryRoute exact path='/blog' component={Blog} />
+								<SentryRoute exact path='/protection' component={Protection} />
+								<SentryRoute exact path='/faqs' component={FAQs} />
+								<SentryRoute exact path='/contact_us' component={ContactUs} />
 
-            {/* post an item */}
-            <AuthRoute path="/postitem" component={PostItem}/>
+								<SentryRoute exact path='/home' component={Home} />
+								<SentryRoute exact path='/item/:itemId' component={ItemPage} />
+								<SentryRoute exact path='/search/:searchParams?' component={Search} />
+								<SentryRoute exact path='/forgotpassword' component={ForgotPassword} />
+								<AuthRoute exact path='/item/edit/:itemId' component={EditItemPage} />
+								<AuthRoute path='/user/trades' component={TradesPage} />
+								<AuthRoute path='/user/messages' component={Messages} />
+								<AuthRoute path='/user/your_shed' component={YourshedPage} />
+								<AuthRoute path='/user/favourites' component={FavouritesPage} />
+								<AuthRoute path='/user/account' component={AccountPage} />
+								<AuthRoute path='/user/update_password' component={UpdatePassword} />
+								{/* if the user is already a lender they should be unable to access the upgrade to lender page */}
+								<RedirectBecomeLender
+									path='/user/upgrade_to_lender'
+									component={UpgradeLender}
+								/>
+								<AuthRoute path='/item/:itemId/application' component={Application} />
 
-            {/* Lender Shed */}
-            <AuthRoute path="/lender-shed/:lenderId" component={LenderShed}/>
+								{/* post an item */}
+								<AuthRoute path='/postitem' component={PostItem} />
 
-            {/* Routes for login/register should redirect to user page if user is logged in */}
-            <AuthRedirectRoute path="/login" component={Login} />
-            <Route path="/register" component={RegisterPage} />
-            
-            {/* <Route path="*" component={<Redirect to={{ pathname: '/' }}/>}/> */}
+								{/* Lender Shed */}
+								<AuthRoute path='/lender-shed/:lenderId' component={LenderShed} />
 
-          </ScrollToTop>
-        </Router>
-        }
-      </GlobalStateContext.Provider>
-      </GlobalErrorContext.Provider>
-    </Elements>
-  )
+								{/* Routes for login/register should redirect to user page if user is logged in */}
+								<AuthRedirectRoute path='/login' component={Login} />
+								<Route path='/register' component={RegisterPage} />
+
+								{/* <Route path="*" component={<Redirect to={{ pathname: '/' }}/>}/> */}
+							</ScrollToTop>
+						</Router>
+					)}
+				</GlobalStateContext.Provider>
+			</GlobalErrorContext.Provider>
+		</Elements>
+	)
 }
 
-export default App;
+export default App
--- src/components/itemCard/itemCard.js
+++ src/components/itemCard/itemCard.js
@@ -1,80 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import './itemCard.css'
 import NoContentImage from '../../assets/Images/NoContent.png'
-import LocationIcon from './../../assets/Icons/LocationIcon.svg';
-import DeliveryIcon from './../../assets/Icons/DeliveryIcon.svg';
-import {ReactComponent as StarOutline} from './../../assets/Icons/StarOutline.svg';
-import {ReactComponent as StarFilled} from './../../assets/Icons/StarFilled.svg';
-import { Link } from 'react-router-dom';
-import getImage from '../../util/getImage';
-import useGlobalState from '../../util/useGlobalState';
+import LocationIcon from './../../assets/Icons/LocationIcon.svg'
+import DeliveryIcon from './../../assets/Icons/DeliveryIcon.svg'
+import { ReactComponent as StarOutline } from './../../assets/Icons/StarOutline.svg'
+import { ReactComponent as StarFilled } from './../../assets/Icons/StarFilled.svg'
+import { Link } from 'react-router-dom'
+import getImage from '../../util/getImage'
+import useGlobalState from '../../util/useGlobalState'
 
-export default function ItemCard({ item, favourited, userId }) {
-  const [ itemImage, setItemImage ] = useState('')
-  const [ itemSuburb, setItemSuburb ] = useState('')
-  const { state } = useGlobalState()
-  const { user } = state
-  
-  useEffect(() => {
-    if (item?.images?.length > 0) {
-      setItemImage(item.images[0]?.imageKey)
-    }
-    if (item?.address?.suburb) {
-      setItemSuburb(item.address?.suburb)
-    }
-  },[item])
+export default function ItemCard({ item, favourited }) {
+	const [itemImage, setItemImage] = useState('')
+	const [itemSuburb, setItemSuburb] = useState('')
+	const { state } = useGlobalState()
+	const { user } = state
 
-  return (
-    <div className="ItemCard">
-      <Link 
-        to={user ? `/item/${item.id}` : '/search'} 
-        style={{position:"relative"}}
-      >
-        <img 
-        src={itemImage 
-          ? getImage(itemImage) 
-          : NoContentImage
-        } 
-        alt={item.title} 
-        className="PreviewImage"
-        />
-      { favourited &&
-        <div className="favouriteStar">
-          <StarFilled/>
-        </div> 
-      }
-      </Link>
-      <div className="ItemDetailsContainer">
-        <div className="ItemNameText">{item.title}</div>
-        <div className="ItemPriceText">${item.price}</div>
+	useEffect(() => {
+		if (item?.images?.length > 0) {
+			setItemImage(item.images[0]?.imageKey)
+		}
+		if (item?.address?.suburb) {
+			setItemSuburb(item.address?.suburb)
+		}
+	}, [item])
 
-        <div className="StatusLocationSection" >
-          <div className="ItemCardIconContainer">
-            <img src={DeliveryIcon} alt='' className="ItemCardIcon"/>
-          </div>
-          {item.deliveryPrice > 0 ? 'Available' : 'Unavailable'}
-        </div>
+	return (
+		<div className='ItemCard'>
+			<Link
+				to={user ? `/item/${item.id}` : '/search'}
+				style={{ position: 'relative' }}>
+				<img
+					src={itemImage ? getImage(itemImage) : NoContentImage}
+					alt={item.title}
+					className='PreviewImage'
+				/>
+				{favourited && (
+					<div className='favouriteStar'>
+						<StarFilled />
+					</div>
+				)}
+			</Link>
+			<div className='ItemDetailsContainer'>
+				<div className='ItemNameText'>{item.title}</div>
+				<div className='ItemPriceText'>${item.price}</div>
 
-        <div className="StatusLocationSection">
-          <div className="ItemCardIconContainer" style={{ paddingLeft: 7 }}>
-            <img src={LocationIcon} alt={itemSuburb} className="ItemCardIcon"/>
-          </div>
-          <div style={{ textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'}}>
-            {itemSuburb}
-          </div>
-        </div>
+				<div className='StatusLocationSection'>
+					<div className='ItemCardIconContainer'>
+						<img src={DeliveryIcon} alt='' className='ItemCardIcon' />
+					</div>
+					{item.deliveryPrice > 0 ? 'Available' : 'Unavailable'}
+				</div>
 
-        <div className="RatingSection">
-          <span className="ItemRateText">{item.rating}/5</span>
-          <div className="RatingStars" style={{ marginLeft: 0 }}>
-            <StarFilled fill='#E9D8B4' className="StarIcon"/>
-            {item.rating >= 2 ? <StarFilled fill='#E9D8B4' className="StarIcon"/> : <StarOutline className="StarIcon"/>}
-            {item.rating >= 3 ? <StarFilled fill='#E9D8B4' className="StarIcon"/> : <StarOutline className="StarIcon"/>}
-            {item.rating >= 4 ? <StarFilled fill='#E9D8B4' className="StarIcon"/> : <StarOutline className="StarIcon"/>}
-            {item.rating >= 5 ? <StarFilled fill='#E9D8B4' className="StarIcon"/> : <StarOutline className="StarIcon"/>}
-            </div>
-        </div>
-      </div>
-    </div>
-  )
+				<div className='StatusLocationSection'>
+					<div className='ItemCardIconContainer' style={{ paddingLeft: 7 }}>
+						<img src={LocationIcon} alt={itemSuburb} className='ItemCardIcon' />
+					</div>
+					<div
+						style={{
+							textOverflow: 'ellipsis',
+							overflow: 'hidden',
+							whiteSpace: 'nowrap',
+						}}>
+						{itemSuburb}
+					</div>
+				</div>
+
+				<div className='RatingSection'>
+					<span className='ItemRateText'>{item.rating}/5</span>
+					<div className='RatingStars' style={{ marginLeft: 0 }}>
+						<StarFilled fill='#E9D8B4' className='StarIcon' />
+						{item.rating >= 2 ? (
+							<StarFilled fill='#E9D8B4' className='StarIcon' />
+						) : (
+							<StarOutline className='StarIcon' />
+						)}
+						{item.rating >= 3 ? (
+							<StarFilled fill='#E9D8B4' className='StarIcon' />
+						) : (
+							<StarOutline className='StarIcon' />
+						)}
+						{item.rating >= 4 ? (
+							<StarFilled fill='#E9D8B4' className='StarIcon' />
+						) : (
+							<StarOutline className='StarIcon' />
+						)}
+						{item.rating >= 5 ? (
+							<StarFilled fill='#E9D8B4' className='StarIcon' />
+						) : (
+							<StarOutline className='StarIcon' />
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
